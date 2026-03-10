3eff40c33098e23a2d191608c893045c4e0606c6
Curtis <cjododev@gmail.com>
2026-03-10 10:48:15 -0600

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Releasing echo-cli (version from git tag)..."
+echo "Releasing echo-cli (version from git tag)...$(git tag -l --sort=-creatordate | head -n 1)"
 goreleaser release --clean
